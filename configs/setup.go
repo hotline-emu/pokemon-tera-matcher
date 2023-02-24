131611493aefa67d6f4d176ea3945dd0537117db
Hotline Emu <11667661+hotline-emu@users.noreply.github.com>
2023-02-24 00:21:40 -0500

    Added env support

--- configs/setup.go
+++ configs/setup.go
@@ -4,21 +4,24 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const MONGO_URI = "mongodb://localhost:27017"
+// Client instance
+var DB *mongo.Client = ConnectDB()
 
 func ConnectDB() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI(MONGO_URI))
+	client, err := mongo.NewClient(options.Client().ApplyURI(getMongoURI()))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 
 	if err != nil {
@@ -30,15 +33,24 @@ func ConnectDB() *mongo.Client {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println("Connected to MongoDB")
+	defer cancel()
+
 	return client
 }
-
-// Client instance
-var DB *mongo.Client = ConnectDB()
 
 // getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	collection := client.Database("PokemonTeraMatcher").Collection(collectionName)
 	return collection
+}
+
+func getMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGO_URI")
 }
